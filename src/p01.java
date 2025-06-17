ce7f4da567f8769f477f5d707a5883ecc9e49e8e
wonjun <dnjswns0010@gmail.com>
2025-06-17 19:48:56 +0900

    별 만들기 1~10번 완료

--- src/p01.java
+++ src/p01.java
@@ -1,4 +1,4 @@
-public class p1 {
+public class p01 {
     public static void main(String[] args) {
         for(int i = 1; i <= 5; i++) {
             System.out.println("*****");
--- src/p02.java
+++ src/p02.java
@@ -1,4 +1,4 @@
-public class p2 {
+public class p02 {
     public static void main(String[] args) {
         for(int i = 1; i <= 5; i++) {
             for(int j = 1; j <= 5; j++) {
