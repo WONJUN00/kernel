1456c7a535a630a987047a8e6cf1834af2f3fd24
wonjun <dnjswns0010@gmail.com>
2025-06-17 18:25:07 +0900

    별 만들기 1~9번 완료

--- src/p9.java
+++ src/p9.java
@@ -2,7 +2,7 @@ public class p9 {
     public static void main(String[] args) {
         int x = 1;
         int y = 5;
-//
+
         for(int i = 1; i <= 3; i++){
             for(int j = 1; j <= 5; j++){
                 if(j >= x && j <= y){
