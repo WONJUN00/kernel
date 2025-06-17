e44e2e08d137175e250b0581af6c258719f9ec0e
wonjun <dnjswns0010@gmail.com>
2025-06-17 18:23:14 +0900

    별 만들기 1~7번 완료

--- src/p8.java
+++ src/p8.java
@@ -2,8 +2,20 @@ public class p8 {
     public static void main(String[] args) {
 
 
-        for(int i = 1; i <= 5; i++){
+        for(int i = 5; i >= 1; i--){
+            for(int j = 1; j <= 5; j++){
+                if(i <= j){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
 
+            for(int j = 1; j <= 5-i; j++){
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
         }
 
     }
---
+++ src/p9.java
@@ -0,0 +1,31 @@
+public class p9 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 5;
+
+        for(int i = 1; i <= 3; i++){
+            for(int j = 1; j <= 5; j++){
+                if(j >= x && j <= y){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            x++;
+            y--;
+            System.out.println(" ");
+        }
+        for(int i = 4; i <= 5; i++){
+            for(int j = 1; j <= 5; j++){
+                if(j <= x && j >= y){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            x++;
+            y--;
+            System.out.println(" ");
+        }
+    }
+}
